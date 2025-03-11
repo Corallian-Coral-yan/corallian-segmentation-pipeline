4fcf66a8ac6290334c5a2a02c56411c36f0772c9
anaj00 <bantolino.jana@gmail.com>
2025-03-11 21:26:19 +0800

    chore: modified num_workers

--- train.py
+++ train.py
@@ -67,10 +67,10 @@ def train(config):
         print(f"Validation dataset size: {len(val_dataset)}")
 
         train_loader = DataLoader(
-            train_dataset, batch_size=config["training"]["BatchSize"], shuffle=True, num_workers=0
+            train_dataset, batch_size=config["training"]["BatchSize"], shuffle=True, num_workers=8
         )
         val_loader = DataLoader(
-            val_dataset, batch_size=config["training"]["BatchSize"], shuffle=False, num_workers=0
+            val_dataset, batch_size=config["training"]["BatchSize"], shuffle=False, num_workers=8
         )
 
         print("DataLoaders created.")
